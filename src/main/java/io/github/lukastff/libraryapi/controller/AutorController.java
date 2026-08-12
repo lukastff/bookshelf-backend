c02a0ce9beb6d57fbe528d09aea2e7120eed5a0d
lukastff <lucastavaresffr@gmail.com>
2026-08-12 09:45:50 -0300

    feat(autor): implement delete functionality for authors
    - Added delete endpoint to AutorController
    - Added deletar method to AutorService to handle repository deletion

--- src/main/java/io/github/lukastff/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/lukastff/libraryapi/controller/AutorController.java
@@ -52,4 +52,18 @@ public class AutorController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id) {
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(autorOptional.get());
+
+        return ResponseEntity.noContent().build();
+    }
 }
