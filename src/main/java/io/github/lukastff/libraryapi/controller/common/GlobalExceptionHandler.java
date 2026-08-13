ea7f0b5e802ad1f28298c837ed1bc50f52fb1f62
lukastff <lucastavaresffr@gmail.com>
2026-08-13 10:07:43 -0300

    feat: implement global exception handler for validation errors

---
+++ src/main/java/io/github/lukastff/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package io.github.lukastff.libraryapi.controller.common;
+
+import io.github.lukastff.libraryapi.controller.dto.ErroCampo;
+import io.github.lukastff.libraryapi.controller.dto.ErroResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErroCampo> listaErros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        System.out.println(e);
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação",
+                listaErros);
+    }
+}
