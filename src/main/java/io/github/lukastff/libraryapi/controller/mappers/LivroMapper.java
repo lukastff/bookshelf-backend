9d9134b070cf0f58e9fe715f9c2f3ab32b0bb27c
lukastff <lucastavaresffr@gmail.com>
2026-08-18 01:10:22 -0300

    feat(livro): implement get book by id endpoint
    - Added `obterPorId` method to `LivroService` to retrieve a book by its UUID.
    - Updated `LivroMapper` to include `toDTO` mapping and configured it to use `AutorMapper`.
    - Added `obterDetalhes` GET endpoint to `LivroController` to return book details as a DTO.

--- src/main/java/io/github/lukastff/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/lukastff/libraryapi/controller/mappers/LivroMapper.java
@@ -1,13 +1,14 @@
 package io.github.lukastff.libraryapi.controller.mappers;
 
 import io.github.lukastff.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.lukastff.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.lukastff.libraryapi.model.Livro;
 import io.github.lukastff.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = AutorMapper.class)
 public abstract class LivroMapper {
 
     @Autowired
@@ -15,4 +16,6 @@ public abstract class LivroMapper {
 
     @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
     public abstract Livro toEntity(CadastroLivroDTO dto);
+
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
